e8067e531b5912711b90103b733f369cf1639a12
Mackenzie <mchoy30@academic.rrc.ca>
2019-04-04 20:31:49 -0500

    Added Images to products table and images to assets folder

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -13,5 +13,5 @@ ActiveAdmin.register Product do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  permit_params :name, :description, :price, :category, :category_id
+  permit_params :name, :description, :price, :category, :category_id, :image
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_04_221451) do
+ActiveRecord::Schema.define(version: 2019_04_05_012541) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -49,10 +49,10 @@ ActiveRecord::Schema.define(version: 2019_04_04_221451) do
     t.string "name"
     t.string "description"
     t.string "price"
-    t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "category_id"
+    t.string "image"
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 

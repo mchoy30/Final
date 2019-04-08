c7ec2be1bdf19f6704ed525dbba09df7be274449
Mackenzie <mchoy30@academic.rrc.ca>
2019-04-08 11:29:32 -0500

    Modified applicating view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_08_010941) do
+ActiveRecord::Schema.define(version: 2019_04_08_062205) do
 
   create_table "abouts", force: :cascade do |t|
     t.string "title"
@@ -90,6 +90,18 @@ ActiveRecord::Schema.define(version: 2019_04_08_010941) do
     t.integer "category_id"
     t.string "image"
     t.index ["category_id"], name: "index_products_on_category_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
